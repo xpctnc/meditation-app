33e86438a8d182ba7f165bd7a292fb4ade0195ba
Adrian Prusik <xpctnc@gmail.com>
2020-08-22 12:57:54 +0200

    Add breathe circle

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
+const techniques = document.querySelector('.techniques')
+const breathe = document.querySelector('.breathe')
+const breatheCircle = document.querySelector('.breathe .breathe-circle');
+const breatheStart = document.querySelector('.breathe span')
+const deep = document.querySelector('.deep h3')
+const box = document.querySelector('.box h3')
+const awake = document.querySelector('.awake h3')
+
+deep.addEventListener('click', () => {
+    techniques.classList.add('hide')
+    breathe.classList.add('active')
+    breatheCircle.classList.add('deep')
+
+})
+
+
 
