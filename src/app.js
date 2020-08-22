e27c0ae1a0fd689ee38aa6208d2ce8820e9f108a
Adrian Prusik <xpctnc@gmail.com>
2020-08-22 20:00:41 +0200

    Add breathing method handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,65 @@
-const techniques = document.querySelector('.techniques')
-const breathe = document.querySelector('.breathe')
-const breatheCircle = document.querySelector('.breathe .breathe-circle');
-const breatheStart = document.querySelector('.breathe span')
-const deep = document.querySelector('.deep h3')
-const box = document.querySelector('.box h3')
-const awake = document.querySelector('.awake h3')
+const container = document.querySelector('.container');
+const returnBtn = document.querySelector('svg.return')
+const breatheCircle = document.querySelector('div.breathe .breathe-circle');
+const breatheCircleSpan = document.querySelector('div.breathe span')
+const deep = document.querySelector('div.deep h3')
+const box = document.querySelector('div.box h3')
+const awake = document.querySelector('div.awake h3')
+
+let method;
 
 deep.addEventListener('click', () => {
-    techniques.classList.add('hide')
-    breathe.classList.add('active')
-    breatheCircle.classList.add('deep')
-
+    container.classList.add('active');
+    breatheCircle.classList.add('deep');
 })
+
+box.addEventListener('click', () => {
+    container.classList.add('active');
+    method = box.textContent;
+    console.log(method);
+})
+
+
+
+awake.addEventListener('click', () => {
+    container.classList.add('active');
+})
+
+
+let bool;
+
+const boxBreathing = () => {
+    setTimeout(() => {
+        bool ? breatheCircleSpan.textContent = 'inhale' : ''
+        setTimeout(() => {
+            bool ? breatheCircleSpan.textContent = 'hold' : ''
+            setTimeout(() => {
+                bool ? breatheCircleSpan.textContent = 'exhale' : ''
+                setTimeout(() => {
+                    bool ? breatheCircleSpan.textContent = 'hold' : ''
+                }, 4000)
+            }, 4000)
+        },4000)
+    },0)
+}
+
+breatheCircleSpan.addEventListener('click', () => {
+    bool = true;
+    breatheCircle.classList.add(method)
+    breatheCircle.classList.contains(method) ? boxBreathing() : ''
+})
+
+
+returnBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+    breatheCircleSpan.textContent = 'start'
+    breatheCircle.classList.remove(method)
+    bool = false;
+})
+
+
+
+
 
 
 
